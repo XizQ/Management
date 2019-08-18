61a65de06612e423f6040887d18d95094780b51b
XizQ <jknap41@interia.pl>
2019-08-18 11:10:28 +0200

    Create Boots Entity

---
+++ src/Boots.java
@@ -0,0 +1,26 @@
+public class Boots extends Product {
+    private int size;
+    private boolean isNaturalSkin;
+
+    public Boots(long id, String productName, float price, float weight, String color, int productCount, int size, boolean isNaturalSkin) {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.isNaturalSkin = isNaturalSkin;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean getIsNaturalSkin() {
+        return isNaturalSkin;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Boots{" +
+                "size='" + size + '\'' +
+                ", isNaturalSkin=" + isNaturalSkin +
+                '}';
+    }
+}
